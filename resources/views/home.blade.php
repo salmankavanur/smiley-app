5ba59aec41621357cf746c3abb2e0c66e45ab799
salmankavanur <mpsalman2011@gmail.com>
2026-01-02 14:46:45 +0530

    whatsapp buttons integrated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -168,6 +168,18 @@
                             <h3>{{ $service->title }}</h3>
                             <p>{{ $service->description }}</p>
                         </div>
+                        <div class="card-footer">
+                            <a href="https://wa.me/919847677300?text={{ urlencode('Hi, I am interested in ' . $service->title . ' service.') }}"
+                                target="_blank" class="btn-wa-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path
+                                        d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z">
+                                    </path>
+                                </svg>
+                                Book via WhatsApp
+                            </a>
+                        </div>
                     </div>
                 @endforeach
             </div>
@@ -185,6 +197,10 @@
                 <div class="cta-actions">
                     <a href="tel:+919847677300" class="btn-black">
                         Call +91 9847 677 300
+                    </a>
+                    <a href="https://wa.me/919847677300?text=Hi, I would like to schedule a pickup." target="_blank"
+                        class="btn-whatsapp-large">
+                        WhatsApp Enquiry
                     </a>
                 </div>
             </div>
